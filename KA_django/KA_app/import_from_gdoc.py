fc47ea9cde1f2438565e1a9b74291d51257cfd36
jbwexler <jbwexler@stanford.edu>
2015-03-12 02:11:36 -0700

    got import working and imported first four brews

--- KA_django/KA_app/import_from_gdoc.py
+++ KA_django/KA_app/import_from_gdoc.py
@@ -9,17 +9,24 @@ def updateBrews(wks):
     brewFields = [field for field in brewLists[2]]
     print len(brewFields)
     for row in brewLists[3:]:
-        try:
-            brewObj = Brew.objects.get(pk=row[0])
-        except:
-            brewObj = Brew.objects.create(pk=row[0])
-        for column, value in enumerate(row):
-            if brewFields[column] in manyToMany:
-                model = get_model('KA_app',brewFields[column])
-                print model
-                addObj = model.objects.get(name=value)
-                getattr(brewObj, brewFields[column]).add(addObj)
-            brewObj.__dict__[brewFields[column]] = value
+        if row[1]:
+            try:
+                brewObj = Brew.objects.get(pk=row[0])
+            except:
+                brewObj = Brew.objects.create(pk=row[0])
+            for column, value in enumerate(row):
+                if value:
+                    if brewFields[column] in manyToMany:
+                        depField = Brew._meta.get_field_by_name(brewFields[column])
+                        model = depField[0].rel.to      
+                        print value         
+                        addObj = model.objects.get(name=value)
+                        getattr(brewObj, brewFields[column]).add(addObj)
+                    elif 'date' in brewFields[column].lower():
+                        dateList = [int(float(x)) for x in value.split('-')]
+                        brewObj.__dict__[brewFields[column]] = datetime.date(dateList[0],dateList[1],dateList[2])
+                    else:
+                        brewObj.__dict__[brewFields[column]] = value
         brewObj.save()
 
 def importFromGDoc():

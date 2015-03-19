7e22080254da07cffb75818c7bfd478edba18445
jbwexler <jbwexler@stanford.edu>
2015-03-18 23:37:11 -0700

    added a test view and url

--- KA_django/KA_django/urls.py
+++ KA_django/KA_django/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+admin.autodiscover()
+from django.contrib.auth.models import User, Group
+from django.http import Http404, HttpResponse
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$', 'KA_django.apps.KA_app.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
